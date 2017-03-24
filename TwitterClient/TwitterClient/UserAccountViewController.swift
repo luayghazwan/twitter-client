02c67335e2cd4b80cb32afd520a511b8b2258450
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-03-23 22:14:35 -0700

    Merged the getAuth user with the override function. A new function creation to convert and Image-url as a String to a valid URL that can be used in swift.

--- TwitterClient/TwitterClient/UserAccountViewController.swift
+++ TwitterClient/TwitterClient/UserAccountViewController.swift
@@ -13,25 +13,36 @@ class UserAccountViewController: UIViewController {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var profileImg: UILabel!
     @IBOutlet weak var userLocation: UILabel!
-    
+    @IBOutlet weak var profileImageView: UIImageView!
     
     var user : User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        userFrom()
-    }
-    
-    func userFrom() {
-        API.shared.getOAuth { (users) in
-            guard let theUser = users else {fatalError("Error grabing the user")}
+        API.shared.getOAuth { (user) in
+            guard let theUser = user else {fatalError("Error grabing the user")}
             OperationQueue.main.addOperation {
                 self.user = theUser
                 self.name.text = "Name \(self.user.name)"
-                self.profileImg.text = "Pic \(self.user.profileImageURL)"
                 self.userLocation.text = "Location \(self.user.location)"
+                self.downloadProfileImage()
+            }
+        }
+    }
+
+    func downloadProfileImage() {
+        OperationQueue.main.addOperation {
+            if let imageURLString = self.user?.profileImageURL {
+                let imageURL = URL(string: imageURLString)
+                if let profileImage = try? UIImage(data: NSData(contentsOf: imageURL!) as Data) {
+                    self.profileImageView.image = profileImage
+                }
             }
         }
     }
 }
+
+// 1. string -> URL
+// 2. get data from url
+// 3. conver data to UIImage
+// 4. put image to imgaeView.image
